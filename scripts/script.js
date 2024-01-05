dc6942a3783dab2530b018abe9cf627105e7dcb8
B251 <ba.sadler1@gmail.com>
2024-01-05 20:23:15 +0000

    added html and CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,13 @@
 var ordinalArr = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th","th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th","th", "st" ];
 
-
-
-const today = dayjs();
-$("#currentDay").text(today.format("dddd,  MMMM DD")+ OrdinalIndicator(today.format("D")));
-
-
+//function to produce ordinal required for date in header not provided by day js
 function OrdinalIndicator(day) {
 return ordinalArr[day];
 }
 
-var ordinalArr = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th","th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th","th", "st" ];
+const today = dayjs();
+$("#currentDay").text(today.format("dddd,"+" "+"MMMM DD")+ OrdinalIndicator(today.format("D")));
+
+
+
+
