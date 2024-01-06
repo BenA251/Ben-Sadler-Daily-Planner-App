cfd21470a5a8f86f517ad3d4e5c5e9df529b961f
B251 <ba.sadler1@gmail.com>
2024-01-06 17:29:58 +0000

    added begin of function which formats colour of cells based on hr

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,9 @@
 var ordinalArr = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th","th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th","th", "st" ];
 
+var divIdArr = [ "6","7","8","9","10","11","12","13","14","15","16","17","18"];
+
+
+
 //function to produce ordinal required for date in header not provided by day js
 function OrdinalIndicator(day) {
 return ordinalArr[day];
@@ -7,6 +11,38 @@ return ordinalArr[day];
 
 const today = dayjs();
 $("#currentDay").text(today.format("dddd,"+" "+"MMMM DD")+ OrdinalIndicator(today.format("D")));
+
+
+function updateCells() {
+ var currentHr = dayjs().format("H")
+  for (i = 0; i < divIdArr.length; i++) {
+    if (divIdArr[i] < currentHr) {
+     $("#"+ divIdArr[i]).addClass("past");
+     $("#"+ divIdArr[i]).removeClass("present");
+     $("#"+ divIdArr[i]).removeClass("future");
+      } 
+    else if (divIdArr[i] > currentHr) {
+     $("#"+ divIdArr[i]).addClass("future");
+     $("#"+ divIdArr[i]).removeClass("past");
+     $("#"+ divIdArr[i]).removeClass("present");
+       } 
+    else {
+      $("#"+ divIdArr[i]).addClass("present");
+      $("#"+ divIdArr[i]).removeClass("past");
+      $("#"+ divIdArr[i]).removeClass("future");
+      }
+  }
+
+}
+
+
+
+
+
+//on start and once document loaded runs the following function
+$( document ).ready(updateCells());
+
+
 
 
 
