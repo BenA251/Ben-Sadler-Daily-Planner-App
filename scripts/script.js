3747c2b0f855e764fed73bd3e7bbfcaa203ece52
B251 <ba.sadler1@gmail.com>
2024-01-06 18:43:25 +0000

    added 1 second interval time for cell colour function so in sync with current time rather than at loading

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 var ordinalArr = ["th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th", "th","th", "st", "nd", "rd", "th", "th", "th", "th", "th", "th","th", "st" ];
 
-var divIdArr = [ "6","7","8","9","10","11","12","13","14","15","16","17","18"];
+var divIdArr = [6,7,8,9,10,11,12,13,14,15,16,17,18];
 
+const today = dayjs();
 
 
 //function to produce ordinal required for date in header not provided by day js
@@ -9,38 +10,35 @@ function OrdinalIndicator(day) {
 return ordinalArr[day];
 }
 
-const today = dayjs();
-$("#currentDay").text(today.format("dddd,"+" "+"MMMM DD")+ OrdinalIndicator(today.format("D")));
-
-
+//function to format cell background according to HR of day using day js and Jquery features
 function updateCells() {
  var currentHr = dayjs().format("H")
   for (i = 0; i < divIdArr.length; i++) {
     if (divIdArr[i] < currentHr) {
-     $("#"+ divIdArr[i]).addClass("past");
-     $("#"+ divIdArr[i]).removeClass("present");
-     $("#"+ divIdArr[i]).removeClass("future");
+    $("#"+ divIdArr[i]).css("background-color", "#d3d3d3");
       } 
     else if (divIdArr[i] > currentHr) {
-     $("#"+ divIdArr[i]).addClass("future");
-     $("#"+ divIdArr[i]).removeClass("past");
-     $("#"+ divIdArr[i]).removeClass("present");
-       } 
+    $("#"+ divIdArr[i]).css("background-color", "#77dd77");
+      } 
     else {
-      $("#"+ divIdArr[i]).addClass("present");
-      $("#"+ divIdArr[i]).removeClass("past");
-      $("#"+ divIdArr[i]).removeClass("future");
+    $("#"+ divIdArr[i]).css("background-color", "#ff6961");
       }
   }
+}
 
+//top level function which runs upon document (html) loading 
+function pageFunction() {
+  updateCells();
+  setInterval(updateCells, 1000);
+  $("#currentDay").text(today.format("dddd,"+" "+"MMMM DD")+ OrdinalIndicator(today.format("D")));
 }
 
 
 
 
 
-//on start and once document loaded runs the following function
-$( document ).ready(updateCells());
+
+$( document ).ready(pageFunction());
 
 
 
